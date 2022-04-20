5f1eec565d1e9326b7fc30fe88c49d0d959fa302
Rambabu Yerajana <rambabuwhy@gmail.com>
2022-04-20 18:34:17 +0530

    Update touppercase.kt

--- touppercase.kt
+++ touppercase.kt
@@ -2,7 +2,14 @@
  * You can edit, run, and share this code.
  * play.kotlinlang.org
  */
+/**
+ * You can edit, run, and share this code.
+ * play.kotlinlang.org
+ */
 fun main(args: Array<String>) {
     val str = args.joinToString(separator = "_"){s: String -> s.uppercase()}
     println(str)
+    
+    val str2 = "rambabu"
+    println(str2.uppercase())
 }
