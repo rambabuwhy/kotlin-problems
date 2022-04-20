3da655121877f752dce453443d14daa387164c40
Rambabu Yerajana <rambabuwhy@gmail.com>
2022-04-20 18:14:47 +0530

    Create hello.kt

---
+++ hello.kt
@@ -0,0 +1,8 @@
+/**
+ * You can edit, run, and share this code.
+ * play.kotlinlang.org
+ */
+fun main(args: Array<String>) {
+    println("Hello, world!!!")
+    println("${args.joinToString()}")
+}
